b98d87926d27cbac89bd99cfc7bbbce2eb78699e
Hcamael <10161996+Hcamael@users.noreply.github.com>
2025-06-25 16:28:07 +0800

    另一种代码复现的方案，但是效果还不行

--- aipyapp/cli/cli_runner.py
+++ aipyapp/cli/cli_runner.py
@@ -8,6 +8,9 @@ from ..aipy.blocks import CodeBlock
 from .. import T, set_lang, __version__
 from ..aipy import TaskManager, ConfigManager, CONFIG_DIR
 
+SYSTEM_PROMPT = """下面我会提供给你 AIPYAPP生成的多个连续的Python代码块，请帮我归纳总结成一份代码文件，并输出。
+"""
+
 class CliRunner:
     def __init__(self, task: 'Task', console: 'Console'):
         self.task = task
@@ -35,7 +38,30 @@ class CliRunner:
                     self.console.print(f"[green]{result[key]}")
                 elif key == "stderr":
                     self.console.print(f"[red]{result[key]}")
-        
+    
+    def run2(self, jsonFile):
+        try:
+            with open(jsonFile) as f:
+                jsonData = json.load(f)
+        except Exception as e:
+            self.console.print("[bold red]Error Json File")
+            return
+        if "blocks" not in jsonData:
+            self.console.print("[bold red]Error Format File, not 'blocks' key.")
+            return
+        blocks = jsonData["blocks"]
+        if not isinstance(blocks, list):
+            self.console.print("[bold red]Error Format File, not list in 'blocks'.")
+            return
+        code = ""
+        n = 1
+        for block in blocks:
+            block = CodeBlock(**block)
+            if block.lang == "python":
+                code += f"# 代码块{n}\n```python\n{block.code}\n```\n"
+                n += 1
+        response = self.task.chat(code, system_prompt=SYSTEM_PROMPT)
+        print(response)
 
 def main(args):
     console = Console(record=True)
